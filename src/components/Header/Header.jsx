8bd30539da0349d5b3e05138e43f79e4517074ee
skmdnurujjaman <smnideal@gmail.com>
2022-03-22 11:36:46 +0530

    API Added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,39 +2,38 @@
 
 import React from "react";
 import { Autocomplete } from "@react-google-maps/api";
-import { AppBar, Toolbar, Typography, Box, InputBase } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
-import useStyles from "./styles";
 
-const Header = () => {
+import useStyles from "./styles.js";
+
+const Header = ({ onPlaceChanged, onLoad }) => {
   const classes = useStyles();
+
   return (
-    <>
-      <AppBar position="static">
-        <Toolbar className={classes.toolbar}>
-          <Typography variant="h5" className={classes.title}>
-            Travel Adviser
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore new places
           </Typography>
-          <Box display="flex">
-            <Typography variant="h6">Explore New Places</Typography>
-            {/* <Autocomplete> */}
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
             <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
               <InputBase
-                placeholder="Search ..."
-                className={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
+                placeholder="Search…"
+                classes={{ root: classes.inputRoot, input: classes.inputInput }}
               />
             </div>
-            {/* </Autocomplete> */}
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </>
+          </Autocomplete>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
